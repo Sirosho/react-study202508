f05dee3bc1a551aaa43285a284d209855d97aa85
Sirosho <pack8447@gmail.com>
2025-08-25 11:24:30 +0900

    하위 컴포넌트에게 props 전달하기

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './ExpenseItem.css';// css 파일의 이름은 컴포넌트와 동일하게, 경로도 같은 곳에 넣는 것이 관례
+import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate.jsx";
+// css 파일의 이름은 컴포넌트와 동일하게, 경로도 같은 곳에 넣는 것이 관례
 
 
 
@@ -9,7 +11,7 @@ const ExpenseItem = ({title, price, date}) => {
 
     return (
         <div className='expense-item'>
-            <div>{date.toLocaleString()}</div>
+            <ExpenseDate expenseDate={date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>{price}원</div>
