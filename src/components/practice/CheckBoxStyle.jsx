0364eadfbb465990d3963c4a768093eb4a45cc0f
Sirosho <pack8447@gmail.com>
2025-08-25 16:38:44 +0900

    연습예제

--- src/components/practice/CheckBoxStyle.jsx
+++ src/components/practice/CheckBoxStyle.jsx
@@ -9,15 +9,30 @@ const CheckBoxStyle = () => {
       3. label의 클래스를 유동적으로 변경해야함.
     */
 
+
+
+       const [isChecked, setIsChecked] = useState('checked');
+
+       const onClick = () => {
+       if(isChecked==='checked') {
+           setIsChecked('unchecked')
+       }
+       else if(isChecked==='unchecked') {
+           setIsChecked('checked');
+       }
+
+    };
+
     return (
         <div className='checkbox-container'>
             <input
                 type='checkbox'
                 id='styled-checkbox'
+                onClick={onClick}
             />
-            <label className='unchecked' htmlFor='styled-checkbox'>Check me!</label>
+            <label className={isChecked } htmlFor='styled-checkbox'>Check me!</label>
         </div>
     );
 };
-
+// return 문에 상태변수가 들어있어야 랜더링에 반영이 됨.
 export default CheckBoxStyle;
