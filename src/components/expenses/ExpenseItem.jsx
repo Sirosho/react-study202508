274ced8f8bb22991cc7a54f2160c549054fd6ec6
Sirosho <pack8447@gmail.com>
2025-08-25 12:49:30 +0900

    상태함수 userState

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate.jsx";
 // css 파일의 이름은 컴포넌트와 동일하게, 경로도 같은 곳에 넣는 것이 관례
@@ -7,11 +7,30 @@ import ExpenseDate from "./ExpenseDate.jsx";
 const ExpenseItem = ({expense}) => {
 
 
+    // 상태변수를 사용하는 useState 훅
+
+    /*
+    useState훅의 리턴값은 배열이며
+    첫번째 요소는 관리할 상태값의 초기값
+    두번째 요소는 해당 상태값을 변경할 때 사용하는 setter함수
+  */
+
     const {title, price, date} = expense;
+
+    const x = useState(title);
+
+    console.log('x', x[0]);
+
+
+// 원화 표기법으로 변환
+    const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
 // 이벤트 핸들러
     const clickHandler = () => {
-        alert('click!!');
+        console.log(`변경 전 : ${x[0]}`);
+        // title = "짜장면";
+        x[1]('짜장면');
+        console.log(`변경 후 : ${x[0]}`);
     }
 
 
@@ -19,12 +38,12 @@ const ExpenseItem = ({expense}) => {
         <div className='expense-item'>
             <ExpenseDate expenseDate={date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>{price}원</div>
+                <h2>{x[0]}</h2>
+                <div className='expense-item__price'>{formatPrice}원</div>
             </div>
 
             <button id='btn-1' onClick={clickHandler}>버튼1</button>
-            <button id='btn-2' onDoubleClick={() => alert('더블클릭!')}>버튼2</button>
+
         </div>
     );
 };
